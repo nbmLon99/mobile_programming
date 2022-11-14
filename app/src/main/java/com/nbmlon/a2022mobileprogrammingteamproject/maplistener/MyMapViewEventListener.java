7601124b402e53a88e683979594d70d7cac812a7
nbmLon99 <rhfwleowkd77@gmail.com>
2022-11-14 16:41:29 +0900

    issue #1 feature : kakao_map 설정

---
+++ app/src/main/java/com/nbmlon/a2022mobileprogrammingteamproject/maplistener/MyMapViewEventListener.java
@@ -0,0 +1,58 @@
+package com.nbmlon.a2022mobileprogrammingteamproject.maplistener;
+
+import net.daum.android.map.MapViewEventListener;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
+
+/** For Map Event Listenr **/
+public class MyMapViewEventListener implements MapView.MapViewEventListener {
+    public MapView.MapViewEventListener getListener(){
+        return this;
+    }
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+    }
+}
---
+++ app/src/main/java/com/nbmlon/a2022mobileprogrammingteamproject/maplistener/MyPOIItemEventListener.java
@@ -0,0 +1,30 @@
+package com.nbmlon.a2022mobileprogrammingteamproject.maplistener;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
+/** For Marker Listener **/
+public class MyPOIItemEventListener implements MapView.POIItemEventListener {
+    public MapView.POIItemEventListener getListener() { return this; }
+    
+    @Override
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+        
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+
+    }
+
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+
+    }
+}
