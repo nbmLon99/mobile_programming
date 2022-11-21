cb5a55d58e1baf0fd029a2ca596b4224dde942e7
nbmLon99 <rhfwleowkd77@gmail.com>
2022-11-22 04:20:39 +0900

    featuer : viewmodel 토대 생성

--- app/src/main/java/com/nbmlon/a2022mobileprogrammingteamproject/viewmodel/PlaceViewModel.java
+++ app/src/main/java/com/nbmlon/a2022mobileprogrammingteamproject/viewmodel/PlaceViewModel.java
@@ -1,4 +1,29 @@
 package com.nbmlon.a2022mobileprogrammingteamproject.viewmodel;
 
-public class PlaceViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.nbmlon.a2022mobileprogrammingteamproject.model.PlaceDTO;
+
+public class PlaceViewModel extends ViewModel {
+
+    public final MutableLiveData<PlaceDTO> placeMutableLiveData = new MutableLiveData<>();
+
+    public PlaceViewModel() {
+        // trigger user load.
+    }
+
+
+    public void searchForCondition(
+            String area,
+
+            int parking,
+            int storage,
+            int infant,
+            int wheel,
+            int pointRoad
+    ) {
+
+    }
+
 }
