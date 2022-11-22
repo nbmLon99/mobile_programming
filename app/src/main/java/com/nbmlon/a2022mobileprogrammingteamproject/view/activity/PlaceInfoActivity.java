9766a64be9fd28694b08c80cd35b386f7ea971c3
nbmLon99 <rhfwleowkd77@gmail.com>
2022-11-22 11:38:27 +0900

    featuer : bundle로부터 데이터 받아오기

--- app/src/main/java/com/nbmlon/a2022mobileprogrammingteamproject/view/activity/PlaceInfoActivity.java
+++ app/src/main/java/com/nbmlon/a2022mobileprogrammingteamproject/view/activity/PlaceInfoActivity.java
@@ -1,16 +1,27 @@
 package com.nbmlon.a2022mobileprogrammingteamproject.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.nbmlon.a2022mobileprogrammingteamproject.R;
+import com.nbmlon.a2022mobileprogrammingteamproject.model.PlaceDTO;
 
 public class PlaceInfoActivity extends AppCompatActivity {
+    PlaceDTO mDstPlace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_info);
+
+        Intent intent = getIntent();
+        mDstPlace = intent.getBundleExtra("placeInfo").getParcelable("placeDTO");
+
+
+
     }
+
+
 }
